6b9cb04cb84d2e400ebf93d36c54aea850e6aef3
JaeWon Heo <wodnjs6371@naver.com>
2021-03-01 22:00:31 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post  #models.py에 정의된 Post모델을 임포트
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')   #모든 Post레코드를 가져와 posts에 저장
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,    #posts를 딕셔너리로 추가
+        }
+    )
+    #render() : 요청이 들어오면 주소를 반환해준다
