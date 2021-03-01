132f5eb9028737e181841c265628af9b260df39a
JaeWon Heo <wodnjs6371@naver.com>
2021-03-02 00:08:57 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,19 @@ def index(request):
 
     return render(
         request,
-        'blog/index.html',
+        'blog/index.html',   #blog/index.html 파일에 화면에 출력할 내용
         {
             'posts': posts,    #posts를 딕셔너리로 추가
+        }
+    )
+def single_post_page(request, pk) :
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post' :post,
         }
     )
     #render() : 요청이 들어오면 주소를 반환해준다
