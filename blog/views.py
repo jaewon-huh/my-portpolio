0d63d4f9583dbd95724e3ec3c06bfd4cd8ffa4e4
JaeWon Heo <wodnjs6371@naver.com>
2021-03-19 22:34:14 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView  #ListView 클래스로 포스트 목록페이지 만들기 +DetailView
-from .models import Post, Category  #models.py에 정의된 Post모델을 임포트
+from .models import Post, Category , Tag #models.py에 정의된 Post모델을 임포트
 
 class PostList(ListView):
     model = Post
@@ -28,6 +28,20 @@ def category_page(request, slug):
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
+        }
+    )
+def tag_page(request, slug):
+        tag = Tag.objects.get(slug=slug)
+        post_list = tag.post_set.all()
+
+        return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'tag': tag,
         }
     )
 
