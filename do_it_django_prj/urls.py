3c5bf9408274c105f4fe444e8edc10b8733928bf
JaeWon Heo <wodnjs6371@naver.com>
2021-03-29 21:08:44 +0900

    구글 로그인 기능 추가하기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls')),  #' ' 안에 공백있으면 에러!
     path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
